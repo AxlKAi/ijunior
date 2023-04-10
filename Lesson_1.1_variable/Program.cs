333cb157112d0d5bb403b15bc0b03999c0a8a885
AxlKAi <alexey.i.krasnov@gmail.com>
2023-04-10 20:03:47 +0300

    first lesson

---
+++ Lesson_1.1_variable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1._1_variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Попрактикуйтесь в создании переменных, объявить 10 переменных разных типов.
+            Напоминание: переменные именуются с маленькой буквы, если название состоит из 
+            нескольких слов, то комбинируем их следующим образом - названиеПеременной.
+            Также имя всегда должно отражать суть того, что хранит переменная.
+            */
+
+            string name;
+            string surname;
+            string livingAddress;
+            string residenceAddress;
+            int passportSerial;
+            int passportNumbeh;
+            float passportIssueDate;
+            float passportExpireDate;
+            bool dualCitizenship;
+            bool male;
+        }
+    }
+}
