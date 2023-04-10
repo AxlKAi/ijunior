2dc7a023f7ff018901440b87c33f7206bf07667b
AxlKAi <alexey.i.krasnov@gmail.com>
2023-04-10 21:03:45 +0300

    Lesson 5

---
+++ Lesson1/Lesson_1.5_Swap_Variable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Даны две переменные. Поменять местами значения двух переменных. 
+            Вывести на экран значения переменных до перестановки и после. 
+            */
+            string name = "Пестропалов";
+            string surname = "Эдуард";
+            string cashe;
+
+            Console.WriteLine($"name={name}; surname={surname};");
+            Console.WriteLine("Меняем местами");
+            cashe = name;
+            name = surname;
+            surname = cashe;
+            Console.WriteLine($"Теперь name={name}; surname={surname};");
+        }
+    }
+}
