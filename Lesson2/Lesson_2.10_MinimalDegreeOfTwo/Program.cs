52439e51026699ca7140bbbd816c3333a95f9f73
AxlKAi <alexey.i.krasnov@gmail.com>
2023-04-14 08:51:21 +0300

    Сделал уроки 2.10 и 2.11

---
+++ Lesson2/Lesson_2.10_MinimalDegreeOfTwo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimalDegreeOfTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int degreeOfCalculatedNumber = 0;
+            int multiplierOfCalculatedNumber = 2;
+            int calculatedNumber = 1;
+            int randomNumber;
+            int randomNumberMaximum = 100000;
+            int randomNumberMinimum = 1;
+            Random random;
+
+            random = new Random();
+            randomNumber = random.Next(randomNumberMinimum, randomNumberMaximum);
+            Console.WriteLine($"Программа найдет минимальную степень двойки, превосходящую число {randomNumber}.");
+
+            while (calculatedNumber <= randomNumber)
+            {
+                calculatedNumber *=  multiplierOfCalculatedNumber;
+                degreeOfCalculatedNumber++;
+            }
+            Console.WriteLine("Степень двойки = {0}; 2^{0} = {1} ", 
+                degreeOfCalculatedNumber, calculatedNumber);
+            Console.WriteLine("Нажмите ENTER для завершения программы.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson2/Lesson_2.11_ParenthesisExpression/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParenthesisExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int parenthesisNestingDegree = 0;
+            string parenthesisExpression;
+            bool isWrongParenthesisExpression = false;
+
+            Console.WriteLine("Программа определит, является ли строка корректным скобочным выражением.");
+            Console.WriteLine("Все символы кроме скобок отбрасываются.");
+            Console.Write("Введите строку из символов \'(\' и \')\':");
+            parenthesisExpression = Console.ReadLine();
+
+            foreach (var parenthesis in parenthesisExpression)
+            {
+                if(parenthesis == '(')
+                {
+                    parenthesisNestingDegree++;
+                }
+                else if(parenthesis == ')')
+                {
+                    parenthesisNestingDegree--;
+                }
+
+                if (parenthesisNestingDegree < 0)
+                {
+                    isWrongParenthesisExpression = true;
+                    break;
+                }
+            }
+
+            if (parenthesisNestingDegree > 0)
+            {
+                isWrongParenthesisExpression = true;
+            }
+
+            if (isWrongParenthesisExpression)
+            {
+                Console.WriteLine("Ошибочное выражение");
+            }
+            else
+            {
+                Console.WriteLine("Правильное выражение");
+            }
+            Console.WriteLine("Нажмите ENTER для завершения программы.");
+            Console.ReadLine();
+        }
+    }
+}
