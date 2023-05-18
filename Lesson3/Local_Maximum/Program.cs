68552000586ead827cb9d4c075b25e89ace90593
AxlKAi <alexey.i.krasnov@gmail.com>
2023-05-18 20:46:14 +0300

    Lesson 3 task 3 little bugs edit

--- Lesson3/Local_Maximum/Program.cs
+++ Lesson3/Local_Maximum/Program.cs
@@ -6,17 +6,53 @@ namespace Local_Maximum
     {
         static void Main(string[] args)
         {
-            int numbersArrayLength = 30;
+            uint numbersArrayLength = 5;
             int maximumRandomNumber = 9;
             int minimumRandomNumber = 0;
+            int previousElement;
+            int nextElement;
+            int currentElement;
 
             int[] numbers = new int[numbersArrayLength];
 
             Random random = new Random();
-            for(int i=0; i<numbers.Length; i++)
+
+            if(numbersArrayLength > 0)
             {
-                numbers[i] = random.Next(minimumRandomNumber, maximumRandomNumber);
-            }
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    numbers[i] = random.Next(minimumRandomNumber, maximumRandomNumber);
+                }
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+
+                Console.WriteLine();
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    currentElement = numbers[i];
+
+                    if (i > 0)
+                        previousElement = numbers[i - 1];
+                    else
+                        previousElement = 0;
+
+                    if (i < (numbers.Length - 1))
+                        nextElement = numbers[i + 1];
+                    else
+                        nextElement = 0;
+
+                    if (currentElement > previousElement && currentElement > nextElement)
+                        Console.WriteLine($"Элемент массива с индексом {i} равный {currentElement} локальный максимум");
+                }
+            } 
+            else
+            {
+                Console.WriteLine("Массив 0-вой величины.");
+            }            
         }
     }
 }
