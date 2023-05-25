150e120b736edd2902cf0c0353a7c721fc122ac7
AxlKAi <alexey.i.krasnov@gmail.com>
2023-05-25 09:29:23 +0300

    Lesson 3.7 first commit

---
+++ Lesson3/Lesson_3.7_String_Word_Split/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3._7_String_Word_Split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textLine = "Хочу создать крутую игру. Fly-экшон в стиле Star Wars.";
+
+            string[] words = textLine.Split(' ');
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.ReadLine();
+        }        
+    }
+}
