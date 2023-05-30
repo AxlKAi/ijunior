5c9cf4e8e296304034658b82dcd8335cb38f6b5d
AxlKAi <alexey.i.krasnov@gmail.com>
2023-05-30 20:08:08 +0300

    4.5 before examine

--- Lesson4/Lesson_4.5_Shuffle/Program.cs
+++ Lesson4/Lesson_4.5_Shuffle/Program.cs
@@ -11,10 +11,10 @@ namespace Lesson_4._5_Shuffle
         static void Main(string[] args)
         {
             string[] words = { "раз", "два", "три", "четыре", "пять" };
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            
             ShowArray<string>(words);
-
             Console.WriteLine();
-            int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
             ShowArray<int>(numbers);
 
             Console.WriteLine("\n\n перемешанные массивы.....");
