20664aaadc576b7383a6f41cbf85f04c2e0d601e
AxlKAi <alexey.i.krasnov@gmail.com>
2023-05-30 06:52:57 +0300

    Lesson 4.3 first commit

---
+++ Lesson4/Lesson_4.3_ReadInt/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4._3_ReadInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int enteredNumber;
+
+            Console.WriteLine("Введите целочисленное значение");
+            enteredNumber = ValidateIntEnter();
+            Console.WriteLine($"Введено значение {enteredNumber}. Нажмите для завершения программы...");
+            Console.ReadKey();
+        }
+
+        static int ValidateIntEnter()
+        {
+            bool isValidateCheckProcess = true;
+            string textInput = "";
+            int convertedInt = 0;
+
+            while (isValidateCheckProcess)
+            {
+                Console.Write(":");
+                textInput = Console.ReadLine();
+                // нужно ли делать принудительно первый символ заглавным ?
+                if (Int32.TryParse(textInput, out convertedInt))
+                {
+                    isValidateCheckProcess = false;
+                }
+                else
+                {
+                    Console.WriteLine("Вводимое значение должно содержать целочисленное значение.");
+                    Console.WriteLine("Строка не может быть пустой.");
+                    Console.WriteLine("Пожалуйста, введите еще раз.");
+                }
+            }
+
+            return convertedInt;
+        }
+    }
+}
