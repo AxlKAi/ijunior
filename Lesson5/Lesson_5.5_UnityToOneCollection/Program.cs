4d94fee32566d178285d488658d34d27e2a4ecef
AxlKAi <alexey.i.krasnov@gmail.com>
2023-06-07 22:12:47 +0300

    5.5 first commit

---
+++ Lesson5/Lesson_5.5_UnityToOneCollection/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson_5._5_UnityToOneCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringsOne = { "1", "2", "3", "4", "5" };
+            string[] stringsTwo = { "1", "2", "6", "7", "8" };
+            List<string> unityStrings = new List<string>();
+
+            MergeElement(unityStrings, stringsOne);
+            MergeElement(unityStrings, stringsOne);
+            ShowElements(unityStrings);
+            Console.ReadKey();
+        }
+
+        static void MergeElement(List<string> unityStrings, string[] strings)
+        {
+            foreach (string element in strings)
+            {
+                if (unityStrings.Contains(element) == false)
+                {
+                    unityStrings.Add(element);
+                }
+            }
+        }
+
+        static void ShowElements(List<string> unityStrings)
+        {
+            foreach(string element in unityStrings)
+            {
+                Console.Write(element + "  ");
+            }
+        }
+    }
+}
