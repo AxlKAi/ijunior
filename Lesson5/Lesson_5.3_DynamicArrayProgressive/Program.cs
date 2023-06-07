9c5c5f5c7673c2fcb5ac6603eeed9d00c5bd8208
AxlKAi <alexey.i.krasnov@gmail.com>
2023-06-07 08:43:46 +0300

    Lesson 5.3 first commit

---
+++ Lesson5/Lesson_5.3_DynamicArrayProgressive/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5._3_DynamicArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string SumCommand = "sum";
+            const string ExitCommand = "exit";
+
+            string userInput;
+            bool isMainLoopActive = true;
+            List<int> numbers = new List<int>();
+
+            Console.WriteLine("Введите:");
+            Console.WriteLine("число - для добавления в массив");
+            Console.WriteLine($"{SumCommand} - для подсчета суммы всех чисел массива");
+            Console.WriteLine($"{ExitCommand} - для выхода");
+            Console.WriteLine();
+
+            while (isMainLoopActive)
+            {
+                Console.Write(":");
+                userInput = Console.ReadLine();
+
+                if (userInput == ExitCommand)
+                {
+                    isMainLoopActive = false;
+                    Console.WriteLine("ВсеГО хо-ро-шЭ-ГО!");
+                }
+                else if (userInput == SumCommand)
+                {
+                    int numbersSum = 0;
+
+                    foreach (int number in numbers)
+                        numbersSum += number;
+
+                    Console.WriteLine($"Сумма элементов массива = {numbersSum}");
+                }
+                else 
+                {
+                    int number = 0;
+
+                    if (Int32.TryParse(userInput,out number))
+                    {
+                        numbers.Add(number);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка ввода, не могу распознать команду или число.");
+                    }                    
+                }
+            }
+        }
+    }
+}
