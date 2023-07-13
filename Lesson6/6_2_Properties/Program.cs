467e21701461c713ebd790705b31367cca184155
AxlKAi <alexey.i.krasnov@gmail.com>
2023-07-13 20:29:51 +0300

    6.2 правки после второй проверки

--- Lesson6/6_2_Properties/Program.cs
+++ Lesson6/6_2_Properties/Program.cs
@@ -1,5 +1,20 @@
 ﻿using System;
 
+/*
+ 
+Доработать немного. Хорошо получилось. 
+1) Renderer showPlayer = new Renderer(10, 10); - не называйте объекты с глагола. [переименовал] 
+    
+
+2) То, что умеете пользоваться разными видами определения свойства - молодец! 
+    В будущем пользуйтесь лаконичными автореализуемыми свойствами :) [принял]
+
+3) MoveTo(int x, int y) - что такое x и y?  Внесите конкретику в именование. [во всех методах переименовал по типу positionX]
+
+4)  GetPosition(out int x, out int y) - Да и в целом вам этот метод не нужен [метод удалил, сделал свойства]
+ 
+ */
+
 namespace _6_2_Properties
 {
     class Program
@@ -7,12 +22,12 @@ namespace _6_2_Properties
         static void Main(string[] args)
         {
             Player player = new Player("AxlUndeadMan", 99, 55);
-            Renderer showPlayer = new Renderer(10, 10);
+            Renderer renderer = new Renderer(10, 10);
 
             player.IncreaseMana(77);
             player.MoveTo(5, 5);
 
-            showPlayer.ShowPlayer(player);
+            renderer.ShowPlayer(player);
 
             Console.ReadKey();
         }
@@ -20,8 +35,8 @@ namespace _6_2_Properties
 
     class Player
     {
-        private int _positionX;
-        private int _positionY;
+        public int PositionX { get; private set; }
+        public int PositionY { get; private set; }
 
         private int _armor = 0;
         private int _maximumArmor = 100;
@@ -59,16 +74,10 @@ namespace _6_2_Properties
 
         public int ManaAmmount { get; private set; }
 
-        public void MoveTo(int x, int y)
+        public void MoveTo(int positionX, int positionY)
         {
-            _positionX = x;
-            _positionY = y;
-        }
-
-        public void GetPosition(out int x, out int y)
-        {
-            x = _positionX;
-            y = _positionY;
+            PositionX = positionX;
+            PositionY = positionY;
         }
 
         public void IncreaseMana(int ammount)
@@ -83,26 +92,24 @@ namespace _6_2_Properties
         private int _windowTopX = 0;
         private int _windowTopY = 0;
 
-        public Renderer(int x, int y)
+        public Renderer(int windowTopLeftX, int windowTopLeftY)
         {
-            _windowTopX = x;
-            _windowTopY = y;
+            _windowTopX = windowTopLeftX;
+            _windowTopY = windowTopLeftY;
         }
 
         public void ShowPlayer(Player player)
         {
             int line = 0;
-            int x;
-            int y;
-
-            player.GetPosition(out x, out y);
+            int positionX = player.PositionX;
+            int positionY = player.PositionY;
 
             Console.SetCursorPosition(_windowTopX, _windowTopY);
             Console.Write($"Игрок {player.Name}");
 
             line++;
             Console.SetCursorPosition(_windowTopX, _windowTopY + line);
-            Console.Write($"Координаты X={x} Y={y}");
+            Console.Write($"Координаты X={positionX} Y={positionY}");
 
             line++;
             Console.SetCursorPosition(_windowTopX, _windowTopY + line);
