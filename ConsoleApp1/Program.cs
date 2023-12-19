1520f5fbc4976977452528b75601e60ef44a3083
AxlKAi <alexey.i.krasnov@gmail.com>
2023-12-19 11:19:54 +0300

    Задание 1.3 мелкие правки

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,15 +10,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-    int tryCount = 5;
-    Random rand = new Random();
-            
-    while (tryCount-- > 0)
-    {
-        Console.WriteLine(tryCount);
-    }
-    // тут tryCount = -1 а не 0 !!
-    Console.WriteLine(tryCount);
+            int tryCount = 5;
+            Random rand = new Random();
+
+            while (tryCount-- > 0)
+            {
+                Console.WriteLine(tryCount);
+            }
+            // тут tryCount = -1 а не 0 !!
+            Console.WriteLine(tryCount);
+
+            int a = 10;
+
+            int b = 38;
+                        
+            int c;
+            c =(31-5 * a) / b ;
+
+            Console.WriteLine(c);
+
         }
     }
 }
--- Lesson1/Lesson_1.3_Strings/Program.cs
+++ Lesson1/Lesson_1.3_Strings/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Strings
 {
@@ -10,12 +6,6 @@ namespace Strings
     {
         static void Main(string[] args)
         {
-            /*
-            Вы задаете вопросы пользователю, по типу "как вас зовут", 
-            "какой ваш знак зодиака" и тд, после чего, по данным, 
-            которые он ввел, формируете небольшой текст о пользователе. 
-            */
-
             string name;
             string city;
             int age;
@@ -23,12 +13,15 @@ namespace Strings
             Console.WriteLine("День добрый, мессир! Разрешите поинтересоваться...");
             Console.Write("Как к Вам обращаться ? Ваше имя? :");
             name = Console.ReadLine();
+
             Console.Write($"В каком городе Вы родились {name} ? :"); ;
-            city = Console.ReadLine();            
+            city = Console.ReadLine();                 
             Console.WriteLine($"Замечательный город {city} !");
-            Console.WriteLine("Сколько Вам лет ? :");
+
+            Console.Write("Сколько Вам лет ? :");
             age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"{age} - это замечательный возраст что бы стать Геймдевом !");
+
+            Console.WriteLine($"{name}!!! {age} - это замечательный возраст, а {city} велеколепное место, что бы стать Геймдевом ! ");
         }
     }
 }
