bf9605f7f17fd55a1bc07ee04826b6f0731117a4
AxlKAi <alexey.i.krasnov@gmail.com>
2023-12-19 15:56:23 +0300

    1.4 убрал коментарий

--- Lesson1/Lesson_1.4_Pictures/Program.cs
+++ Lesson1/Lesson_1.4_Pictures/Program.cs
@@ -10,13 +10,6 @@ namespace Pictures
     {
         static void Main(string[] args)
         {
-            /*
-            На экране, в специальной зоне, выводятся картинки, 
-            по 3 в ряд (условно, ничего рисовать не надо). 
-            Всего у пользователя в альбоме 52 картинки. 
-            Код должен вывести, сколько полностью заполненных рядов можно будет вывести, 
-            и сколько картинок будет сверх меры.             
-            */
             int picturesInRow = 3;
             int totalPictures = 52;
             int overMeasureRowPictures = totalPictures % picturesInRow;
