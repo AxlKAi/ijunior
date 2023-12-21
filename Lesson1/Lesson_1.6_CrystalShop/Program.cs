a39144587882dda613d6d32285792af749ceb209
AxlKAi <alexey.i.krasnov@gmail.com>
2023-12-21 13:23:41 +0300

    1.6 переименовал переменную

--- Lesson1/Lesson_1.6_CrystalShop/Program.cs
+++ Lesson1/Lesson_1.6_CrystalShop/Program.cs
@@ -12,16 +12,16 @@ namespace Lesson_1._6_CrystalShop
         {
             int walletAmmount;
             int crystalCost = 12;
-            int purchasedCrystal;
+            int crystals;
 
             Console.WriteLine("Вы находитесь в магазине кристалов.");
             Console.Write("Введите сумму денег которую вы с собой взяли:");
             walletAmmount = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"Стоимость одного кристала {crystalCost}");
             Console.Write("Введите сколько кристалов хотите купить:");
-            purchasedCrystal = Convert.ToInt32(Console.ReadLine());
-            walletAmmount -= purchasedCrystal * crystalCost;
-            Console.WriteLine($"Вы преобрели {purchasedCrystal} кристалов, в кошельке осталось {walletAmmount}");
+            crystals = Convert.ToInt32(Console.ReadLine());
+            walletAmmount -= crystals * crystalCost;
+            Console.WriteLine($"Вы преобрели {crystals} кристалов, в кошельке осталось {walletAmmount}");
         }
     }
 }
