379dd7bc1dea0e00d1bb9a36bf4db5a564ef5ed4
AxlKAi <36095751+AxlKAi@users.noreply.github.com>
2024-01-10 08:51:06 +0300

    сделал уневерсальный вывод для любой степени

--- Lesson2/Lesson_2.10_MinimalDegreeOfTwo/Program.cs
+++ Lesson2/Lesson_2.10_MinimalDegreeOfTwo/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MinimalDegreeOfTwo
 {
@@ -20,15 +16,17 @@ namespace MinimalDegreeOfTwo
 
             random = new Random();
             randomNumber = random.Next(randomNumberMinimum, randomNumberMaximum);
-            Console.WriteLine($"Программа найдет минимальную степень двойки, превосходящую число {randomNumber}.");
+            Console.WriteLine($"Программа найдет минимальную степень числа {multiplierOfCalculatedNumber}, превосходящую число {randomNumber}.");
 
             while (calculatedNumber <= randomNumber)
             {
                 calculatedNumber *=  multiplierOfCalculatedNumber;
                 degreeOfCalculatedNumber++;
             }
-            Console.WriteLine("Степень двойки = {0}; 2^{0} = {1} ", 
-                degreeOfCalculatedNumber, calculatedNumber);
+
+            Console.WriteLine($"Степень числа {multiplierOfCalculatedNumber} = {degreeOfCalculatedNumber}; " +
+                $"{multiplierOfCalculatedNumber}^{degreeOfCalculatedNumber} = {calculatedNumber} ");
+            
             Console.WriteLine("Нажмите ENTER для завершения программы.");
             Console.ReadLine();
         }
