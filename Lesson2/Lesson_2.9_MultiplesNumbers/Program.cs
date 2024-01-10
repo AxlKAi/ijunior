7737bcf8ee07cb7ec458a4b7fdaa2423c2eecf30
AxlKAi <36095751+AxlKAi@users.noreply.github.com>
2024-01-10 08:39:38 +0300

    немного подправил вывод

--- Lesson2/Lesson_2.9_MultiplesNumbers/Program.cs
+++ Lesson2/Lesson_2.9_MultiplesNumbers/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MultiplesNumbers
 {
@@ -11,7 +7,7 @@ namespace MultiplesNumbers
         static void Main(string[] args)
         {
             int multiplieNumber;
-            int maxMultiplieNumberPlusOne = 28;
+            int maxMultiplieNumber = 27;
             int minMultiplieNumber = 1;
             int calculationMaxNumber = 999;
             int calculationMinNumber = 100;
@@ -19,21 +15,22 @@ namespace MultiplesNumbers
             Random random;
 
             random = new Random();
-            multiplieNumber = random.Next(minMultiplieNumber, maxMultiplieNumberPlusOne);
+            multiplieNumber = random.Next(minMultiplieNumber, maxMultiplieNumber + 1);
             Console.WriteLine($"Программа выведет сумму чисел кратных {multiplieNumber} от {calculationMinNumber} до {calculationMaxNumber} не прибегая к умножению и делению.\n"); ;
-            Console.WriteLine("Ловкость рук, никакого мошенства!!!\n");            
+            Console.WriteLine("Ловкость рук, никакого мошенства!!!\n");
             Console.WriteLine("Числа попадающие в подсчет : ");
-            
+
             for (int i = 0; i <= calculationMaxNumber; i = i + multiplieNumber)
             {
                 if (i >= calculationMinNumber)
                 {
-                    multipliesSum += i;                    
-                    Console.Write(i+ " ");
+                    multipliesSum += i;
+                    Console.Write(i + " ");
                 }
             }
-            Console.WriteLine($"\nСумма = {multipliesSum}");
-            Console.WriteLine("\nНажмите ENTER для завершение программы.");
+
+            Console.WriteLine($"\nСумма = {multipliesSum}\n");
+            Console.WriteLine("Нажмите ENTER для завершение программы.");
             Console.ReadLine();
         }
     }
