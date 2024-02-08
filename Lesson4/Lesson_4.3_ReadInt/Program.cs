6bbaf855349b38839e14340b91e5a2c72c720392
AxlKAi <36095751+AxlKAi@users.noreply.github.com>
2024-02-08 15:43:00 +0300

    сделал задание через главный цикл, так более читаемо

--- Lesson4/Lesson_4.3_ReadInt/Program.cs
+++ Lesson4/Lesson_4.3_ReadInt/Program.cs
@@ -6,27 +6,26 @@ namespace Lesson_4._3_ReadInt
     {
         static void Main(string[] args)
         {
-            int enteredNumber;
+            int enteredNumber = 0;
+            bool isNumberEntered = false;
 
             Console.WriteLine("Введите целочисленное значение.");
             Console.WriteLine("Вводимое значение должно содержать целочисленное значение.");
             Console.WriteLine("Строка не может быть пустой.");
-            enteredNumber = ReadInt();
+
+            while (isNumberEntered == false)
+            { 
+                Console.Write("Введите число:");
+                isNumberEntered = TryReadInt(ref enteredNumber);
+            } 
 
             Console.WriteLine($"Введено значение {enteredNumber}. Нажмите для завершения программы...");
             Console.ReadKey();
         }
 
-        static int ReadInt()
+        static bool TryReadInt(ref int enteredNumber)
         {
-            int number;
-
-            do
-            {
-                Console.Write("Введите число:");
-            } while (Int32.TryParse(Console.ReadLine(), out number) == false);
-
-            return number;
+            return Int32.TryParse(Console.ReadLine(), out enteredNumber);
         }
     }
 }
