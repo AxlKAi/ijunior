89fa863fffd919e4746b71c144dea36e31cb8fae
AxlKAi <alexey.i.krasnov@gmail.com>
2025-09-08 14:39:34 +0300

    save

---
+++ Lesson6/GIU_Terminal/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Terminal.Gui;
+
+namespace GIU_Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.Init();
+            var top = Application.Top;
+
+            var login = new Label("Login") { X = 5, Y = 5 };
+            top.Add(login);
+
+
+            var win = new Window("MyApp")
+            {
+                X = 0,
+                Y = 1,
+
+                Width = 30,
+                Height = 25
+            };
+            top.Add(win);
+
+            Application.Run();
+        }
+    }
+}
